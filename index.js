a47a5721ff0e7de2862e03d87eac4ec89afe3e4b
Leif Dalan <leifdalan@gmail.com>
2019-06-12 00:13:48 -0700

    Ready!

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ const fs = require('fs');
 const speedTest = require('speedtest-net');
 const ping = require('ping');
 
+let successfulSpeedtests = 0;
+let errorSpeedtests = 0;
+let successfulPings = 0;
+let errorPings = 0;
+
+let cumulativeDownload = 0;
+let cumulativeUpload = 0;
+let cumulativePing = 0;
+
 const speedInterval = setInterval(() => {
   const test = speedTest();
   test.on('data', data => {
+    successfulSpeedtests += 1;
+    cumulativeDownload += data.speeds.download;
+    cumulativeUpload += data.speeds.upload;
     fs.appendFileSync(
       'speed.txt',
       `\n${new Date().toLocaleTimeString()} - ${data.speeds.download} / ${
@@ -14,6 +26,7 @@ const speedInterval = setInterval(() => {
     );
   });
   test.on('error', error => {
+    errorSpeedtests += 1;
     fs.appendFileSync(
       'speed.txt',
       `\n${new Date().toLocaleTimeString()} - ${JSON.stringify(error)}`,
@@ -24,14 +37,40 @@ const speedInterval = setInterval(() => {
 const pingInterval = setInterval(async () => {
   try {
     const res = await ping.promise.probe('google.com');
-    fs.appendFileSync(
-      'ping.txt',
-      `\n${new Date().toLocaleTimeString()} - ${res.time}ms`,
-    );
+    if (res.alive) {
+      successfulPings += 1;
+      cumulativePing += res.time;
+      fs.appendFileSync(
+        'ping.txt',
+        `\n${new Date().toLocaleTimeString()} - ${res.time}ms`,
+      );
+    } else {
+      throw res;
+    }
   } catch (e) {
+    errorPings += 1;
     fs.appendFileSync(
       'ping.txt',
       `\n${new Date().toLocaleTimeString()} - ERROR! ${JSON.stringify(e)}`,
     );
   }
+}, 1000);
+
+const reportInterval = setInterval(() => {
+  const successRatePings = successfulPings / (successfulPings + errorPings);
+  const successRateSpeedtests =
+    successfulSpeedtests / (successfulSpeedtests + errorSpeedtests);
+  const averageDownload = cumulativeDownload / successfulSpeedtests;
+  const averageUpload = cumulativeUpload / successfulSpeedtests;
+  const averagePing = cumulativePing / successfulPings;
+  fs.appendFileSync(
+    'report.txt',
+    `\n${new Date().toLocaleTimeString()} - 
+Ping success rate: ${successRatePings}
+Speedtest success rate: ${successRateSpeedtests}
+Average Download: ${averageDownload}
+Average Upload = ${averageUpload}
+Average Ping = ${averagePing}
+`,
+  );
 }, 1000);
